453f12b04a7407fcddb0a7017e055c3b9c472acc
mchitic <37516505+mchitic@users.noreply.github.com>
2018-04-30 11:55:08 +0300

    Add files via upload

--- Adaugare categorie/AdaugareCategorieV1.0.js
+++ Adaugare categorie/AdaugareCategorieV1.0.js
@@ -1,18 +1,18 @@
 window.onload=function(){
 	
-	var titluAnunt = document.getElementById("Titlu");
+	var titluCategorie = document.getElementById("Titlu");
 	var wordCounterTitlu = document.getElementById("wordCounterTitlu");
 	
-	var descriereAnunt = document.getElementById("Descriere");
+	var descriereCategorie = document.getElementById("Descriere");
 	var wordCounterDescriere = document.getElementById("wordCounterDescriere");
 	
 	titluAnunt.addEventListener("keyup",function(){
-		var caractere = titluAnunt.value.split('');
+		var caractere = titluCategorie.value.split('');
 		wordCounterTitlu.innerText = 70 - caractere.length;
 	});	
 	
 	descriereAnunt.addEventListener("keyup",function(){
-		var caractere = descriereAnunt.value.split('');
+		var caractere = descriereCategorie.value.split('');
 		wordCounterDescriere.innerText = 4096 - caractere.length;
 	});	
 	
